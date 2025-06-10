010c07720e1fb0c12914289e6204828d665df38a
nabiii2003 <ariyatakeya@gmail.com>
2025-06-11 04:36:39 +0900

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-$('.burger').click(function(){
-    $('.menu').toggleClass('is-active');
-    $('.burger').toggleClass('is-active');
-    $('.navi-list').toggleClass('is-active');
-});
-
-});
